8f54f5a79bb13b31fb4036bc86e55f41fefa7b5e
Nicolas Cavallin <nicavallin@gmail.com>
2022-03-21 10:26:12 +0100

    redeploy fix

--- src/components/Sort/Sort.ts
+++ src/components/Sort/Sort.ts
@@ -4,7 +4,8 @@ const Sort = styled.button`
   background: ${(props: { selected: boolean }) =>
     props.selected
       ? "linear-gradient(102.47deg, #176feb -5.34%, #ff80ff 106.58%)"
-      : props.theme.colors.brand.light};
+      : // @ts-ignore
+        props.theme.colors.brand.light};
   border-radius: 12px;
   padding: 8px 24px;
   width: auto;
@@ -14,7 +15,8 @@ const Sort = styled.button`
     background: ${(props: { selected: boolean }) =>
       props.selected
         ? "linear-gradient(102.47deg, #1667d9 -5.34%, #f279f2 106.58%)"
-        : props.theme.colors.brand.light2};
+        : // @ts-ignore
+          props.theme.colors.brand.light2};
   }
   &:active {
     opacity: 0.8;
